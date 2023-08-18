7f664f9024a8fd0ad7e481ad8ea8eefa6caae70e
mksmushkv <dopemaksjr@mail.ru>
2023-08-18 16:35:59 +0500

    выполнил 3 дз

---
+++ script/hw3.js
@@ -0,0 +1,117 @@
+//Задание 1
+// let password = 'Пароль';
+// let password2 = prompt ('Введите пароль');
+// if (password === password2) {
+//     alert ('Пароль верный');
+// } else {
+//     alert ('Пароль не верный');
+// }
+//Задание 2
+// let c = 1;
+// if (c > 0 && c < 10) {
+//     alert ('Верно1');
+// } else {
+//     alert ('Не верно1');
+// };
+// c = 10;
+// if (c > 0 && c < 10) {
+//     alert ('Верно2');
+// } else {
+//     alert ('Не верно2');
+// };
+// c = -3;
+// if (c > 0 && c < 10) {
+//     alert ('Верно3');
+// } else {
+//     alert ('Не верно3');
+// };
+// c = 2;
+// if (c > 0 && c < 10) {
+//     alert ('Верно4');
+// } else {
+//     alert ('Не верно4');
+// };
+
+//Задание 3
+// let d = 55;
+// let e = 155;
+// if ( d > 100 || e > 100) {
+// alert ('Верно');
+// } else {
+//     alert ('Не верно')};
+
+//Задание 4
+// let a = '2';
+// let b = '3';
+
+// alert(Number(a) + Number(b));
+
+//Задание 5
+// let number = parseInt(prompt('Номер месяца'));
+// if(number < 13 && number > 1){
+// switch(number) {
+//     case 3:
+//     case 4:
+//     case 5:
+//     console.log('это весна');
+
+// };
+// switch(number) {
+//     case 6:
+//     case 7:
+//     case 8:
+//     console.log('это лето');
+
+// };
+// switch(number) {
+//     case 9:
+//     case 10:
+//     case 11:
+//  console.log('это осень');
+
+// };
+// switch(number) {
+//     case 12:
+//     case 1:
+//     case 2:
+//  console.log('это зима');
+
+// };
+// } else {
+//     alert('нет такого месяца')
+// };
+
+//Задание 7
+// let f = parseInt(prompt ('введите любое число'));
+// if(f % 2 == 0){
+//     alert("четное");
+// } else {
+//     alert("нечетное");
+// };
+
+//Задание 8
+// var clientOS = 0; 
+// if (clientOS === 0) {
+//   console.log("Установите версию приложения для iOS по ссылке");
+// } else if (clientOS === 1) {
+//   console.log("Установите версию приложения для Android по ссылке");
+// } else {
+//   console.log("Неизвестная операционная система");
+// }
+
+//Задание 9
+// let clientOS = 0; 
+// let clientDeviceYear = 2001; 
+// if (clientDeviceYear >= 2015) {
+//   if (clientOS === 0) {
+//     console.log("Установите приложение для iOS по ссылке");
+//   } else if (clientOS === 1) {
+//     console.log("Установите приложение для Android по ссылке");
+//   }
+// } else {
+//   if (clientOS === 0) {
+//     console.log("Установите облегченную версию приложения для iOS по ссылке");
+//   } else if (clientOS === 1) {
+//     console.log("Установите облегченную версию приложения для Android по ссылке");
+//   }
+// };
